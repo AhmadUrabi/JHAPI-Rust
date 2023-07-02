0de35de35debfe76ab856246e9a20ece886017d6
Ahmad urabi <hamod9041@gmail.com>
2023-07-02 15:03:50 +0300

    Query with pooling + cleaner code + Basic Timelog

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+use oracle::pool::Pool;
+
+use rocket::{State, post};
+
+
+use oracle::pool::PoolBuilder;
+use rocket::serde::json::Json;
+
+
+mod getproductdata;
+mod apistructs;
+
+
+use crate::getproductdata::get_product_data;
+use crate::apistructs::FetchParams;
+use crate::apistructs::Product;
+
+#[launch]
+fn rocket() -> _ {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+    let pool = PoolBuilder::new(username, password, database)
+    .max_connections(20)
+    .build();
+
+    let pool = match pool {
+        Ok(pool) => pool,
+        Err(err) => panic!("Error Creating Pool: {}", err.to_string()),
+    };
+    rocket::build().manage(pool).mount("/", routes![get_products])
+}
+
+#[post("/GetProductData", data = "<params>")]
+async fn get_products(params: Json<FetchParams>, pool: &State<Pool>) -> Option<Json<Vec<Product>>> {
+    println!("Time on request: {:?}", SystemTime::now().duration_since(UNIX_EPOCH));
+    return get_product_data(params, pool).await;
 }
