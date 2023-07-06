0d0de717ddbf62225af5c94c878d1a875a655343
Ahmad urabi <hamod9041@gmail.com>
2023-07-06 10:15:07 +0300

    Some Error Handling + Clean Up

--- src/fetchstores/mod.rs
+++ src/fetchstores/mod.rs
@@ -1,26 +1,20 @@
 use oracle::{Result};
 use oracle::pool::Pool;
-use rocket::serde::json::Json;
 
 use crate::apistructs::Store;
 
-pub async fn fetch_store_list(pool: &Pool) -> Option<Json<Vec<Store>>> {
-    println!("Fetching Store List");
-    let stores = get_stores(pool).unwrap();
-    if stores.is_empty() {
-        return None;
-    } else {
-        Some(Json(stores.into_iter().map(|store| Store {
-            STORE_ID: store.STORE_ID,
-            STORE_DESC: store.STORE_DESC,
-            STORE_DESC_S: store.STORE_DESC_S,
-        }).collect()
-        ))
-    }
+pub async fn fetch_store_list(pool: &Pool) -> Vec<Store> {
+    match get_stores(pool) {
+        Ok(stores) => stores,
+        Err(err) => {
+            println!("Error: {}", err.to_string());
+            Vec::new()
+        }
+    }  
 }
 
 fn get_stores(pool: &Pool) -> Result<Vec<Store>> {
-    let mut conn = pool.get()?;
+    let conn = pool.get()?;
     let mut stmt = conn.statement("SELECT STORE_ID, STORE_DESC, STORE_DESC_S FROM ODBC_JHC.JHC_STORES").build()?;
     let rows = stmt.query(&[])?;
     let mut stores: Vec<Store> = Vec::new();
