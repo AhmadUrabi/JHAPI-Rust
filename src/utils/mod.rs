22b81c06c5091d8f6e33e11aff31d9a5fa322154
Ahmad urabi <hamod9041@gmail.com>
2023-09-05 11:22:36 +0300

    Clean Up 1

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -13,3 +13,33 @@ pub fn is_admin_perm(_key: &ApiKey<'_>, pool: &Pool) -> bool {
     }
     false
 }
+
+pub fn is_perm_perm(_key: &ApiKey<'_>, pool: &Pool) -> bool {
+    let user_id = decode_token_data(_key.0).unwrap().USER_ID.unwrap();
+    let permissions: Vec<String> = get_user_permissions(&user_id, pool).unwrap();
+    if permissions.contains(&"permissions".to_string()){
+        println!("Perm Permissions Found");
+        return true;
+    }
+    false
+}
+
+pub fn is_users_perm(_key: &ApiKey<'_>, pool: &Pool) -> bool {
+    let user_id = decode_token_data(_key.0).unwrap().USER_ID.unwrap();
+    let permissions: Vec<String> = get_user_permissions(&user_id, pool).unwrap();
+    if permissions.contains(&"users".to_string()){
+        println!("Users Permissions Found");
+        return true;
+    }
+    false
+}
+
+pub fn is_images_perm(_key: &ApiKey<'_>, pool: &Pool) -> bool {
+    let user_id = decode_token_data(_key.0).unwrap().USER_ID.unwrap();
+    let permissions: Vec<String> = get_user_permissions(&user_id, pool).unwrap();
+    if permissions.contains(&"images".to_string()){
+        println!("Images Permissions Found");
+        return true;
+    }
+    false
+}
